63a3183e4452d1cf8821894d654fe2571fa56b1a
Nacho Álvarez Ruiz <neonigma@gmail.com>
2012-07-07 18:41:29 +0200

    Licensing code

--- test.py
+++ test.py
@@ -1,4 +1,37 @@
-#!/usr/bin/python
+# This file is part of Persistence Systems
+#
+# This software is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""
+  A brief comparison into persistence systems for Python
+
+  Currently comparing following systems:
+  + bsddb
+  + pytables
+  + durus
+  + zodb
+  + redis
+
+  This module contains the main program that executes tests reading
+  and writing process, measuring time to end both read and write process
+"""
+
+__author__ = "Jose I. Alvarez <neonigma@gmail.com>"
+__copyright__ = "Copyright 2011, Jose I. Alvarez <neonigma@mail.com>"
+__license__ = "GPL-2"
+
 import csv
 import time
 import getopt
